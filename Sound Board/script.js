4a0230d06ffaff827a57ce2163cd46e91e635b4f
Murali03M <murali732000@gmail.com>
2022-03-18 20:01:59 +0530

    sound board

--- Sound Board/script.js
+++ Sound Board/script.js
@@ -1,8 +1,24 @@
 const sounds=['fida','mara','mankatha','polla'];
-sounds.forEach(sound =>
-    {
-        const btn=document.getElementById('button');
-        btn.classList.add('btn');
-        btn.innerText=sound;
-        document.getElementById('buttons').appendChild(btn);
+sounds.forEach(sound => {
+    const btn = document.createElement('buttons')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
     })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
